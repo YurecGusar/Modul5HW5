a4c3b146f862fd2135ebf4f993e8e5fc39c4659b
Yurii <leoonovn@gmail.com>
2021-10-08 12:36:57 +0300

    Добавил макет навигационной панели

---
+++ modul5hw5/src/components/HomeComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export class HomeComponent extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        return <div class="Top-bar">
+            <div>Register</div>
+            <div>Game</div>
+            <div>Cars</div>
+            </div>
+    }
+}
